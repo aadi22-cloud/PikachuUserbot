5966b2364c1dfd423a8a35239abcd81b9eb8ce22
Sachin Jangir <sachinjangir022@gmail.com>
2020-08-19 03:05:10 +0530

    Update __init__.py

--- pikabot/__init__.py
+++ pikabot/__init__.py
@@ -15,10 +15,10 @@ else:
 
 if Var.STRING_SESSION2:
     session_name = str(Var.STRING_SESSION2)
-    Bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    botx = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup2"
-    Bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+    botx = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
 
 CMD_LIST = {}
