afb8b031dfaa72c5063e3a7ff2adfbf4e801b0a4
Sachin Jangir <sachinjangir022@gmail.com>
2020-08-19 11:59:58 +0530

    Update __init__.py

--- pikabot/__init__.py
+++ pikabot/__init__.py
@@ -3,12 +3,12 @@ from logging import basicConfig, getLogger, INFO, DEBUG, WARNING ; from distutil
 from pySmartDL import SmartDL
 from requests import get
 
-client2 = None
 if STRING_SESSION:
+    client2 = TelegramClient(StringSession(Var.STR2),Var.APP_ID,Var.API_HASH,connection_retries=None,auto_reconnect=False,lang_code='en')
     bot = TelegramClient(StringSession(Var.STRING_SESSION),Var.APP_ID,Var.API_HASH,connection_retries=None,auto_reconnect=False,lang_code='en')
 else:
      quit(1)
-client2 = TelegramClient(StringSession(Var.STR2),Var.APP_ID,Var.API_HASH,connection_retries=None,auto_reconnect=False,lang_code='en')
+
 
 
 
