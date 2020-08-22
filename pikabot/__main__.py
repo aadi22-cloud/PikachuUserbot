bdc7e77ba9e4e0f8a6c1c96d58adc4c336bfdc81
Sachin Jangir <sachinjangir022@gmail.com>
2020-08-22 22:58:32 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -1,4 +1,31 @@
 import os, telethon, telethon.utils, asyncio, traceback ; from pikabot import * ; from sys import * ; from var import * ; client = bot ; ItzSjDude = client ; from telethon.errors.rpcerrorlist import * ; from var import Client as clIent ; from pathlib import Path ; from telethon import * ; from telethon.tl.types import * 
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        bot.tgbot = TelegramClient("TG_BOT_TOKEN",api_id=Var.APP_ID,api_hash=Var.API_HASH).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+    else:
+        bot.start()
+async def add_bot2(bottoken):
+     await bot.start(bottoken)
+     bot.me = await bot.get_me()
+     bot.uid = telethon.utils.get_peer_id(bot.me)
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot2 = None
+    if Var.TG_BOT2_USER_NAME is not None:
+       bot.tgbot2 = TelegramClient("TG_BOT_TOKEN2",api_id=Var.APP_ID,api_hash=Var.API_HASH).start(bottoken=Var.TG_BOT_USER_NAME)
+       bot.loop.run_until_complete(add_bot2(Var.TG_BOT_USER_NAME)
+    else:
+        bot.start()
+
 async def alt():
     await bot.start()
     LOGS.info("Detecting nd Connecting to Sessions...")
@@ -27,33 +54,6 @@ async def alt():
     for ixo in total_doxx:
        mxo =cli1[ixo].id ; await client.download_media(await bot.get_messages(clIent, ids=mxo), "pikabot/main_plugs")
 bot.loop.run_until_complete(alt())
-
-async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me() 
-    bot.uid = telethon.utils.get_peer_id(bot.me)
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.tgbot = None
-    if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        bot.tgbot = TelegramClient("TG_BOT_TOKEN",api_id=Var.APP_ID,api_hash=Var.API_HASH).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-    else:
-        bot.start()
-async def add_bot2(bottoken):
-     await bot.start(bottoken)
-     bot.me = await bot.get_me()
-     bot.uid = telethon.utils.get_peer_id(bot.me)
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.tgbot2 = None
-    if Var.TG_BOT2_USER_NAME is not None:
-       bot.tgbot2 = TelegramClient("TG_BOT_TOKEN2",api_id=Var.APP_ID,api_hash=Var.API_HASH).start(bottoken=Var.TG_BOT_USER_NAME)
-       bot.loop.run_until_complete(add_bot2(Var.TG_BOT_USER_NAME)
-    else:
-        bot.start()
 
 print("Initialising Core")
 
